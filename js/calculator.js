47ceb391c135f58db4f8151b5f68c78b0392ca64
Myriam <salazarmyriarg@gmail.com>
2020-06-15 02:11:25 -0300

    se agrego carpeta js

---
+++ js/calculator.js
@@ -0,0 +1,10 @@
+document.getElementById("n1");
+document.getElementById("n2");
+document.getElementById("n3");
+document.getElementById("n4");
+document.getElementById("n5");
+document.getElementById("n6");
+document.getElementById("n7");
+document.getElementById("n8");
+document.getElementById("n9");
+document.getElementById("n0");
